f099ac4fa9aeb5cba128867cb1eaa671aaf6b5fc
Chris van der Ploeg <chris@chrisvdp.com>
2016-12-29 15:19:59 -0700

    Display message when success is returned from paypal

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -39,7 +39,26 @@ jQuery(document).foundation();
   "use strict";
   $(document).ready(function() {
 
-    $('video').each(function() {
+    var getUrlParameter = function getUrlParameter(sParam) {
+      var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+      for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+          return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+      }
+    };
+    var paid = getUrlParameter('paid');
+    if (paid) {
+      $('#payment-success').show();
+    }
+
+    $('video').each(function () {
       this.muted = true;
     });
 
